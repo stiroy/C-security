97bcafef21237db841ad1840d7fd4709d2c0e9ef
stiroy <55147197+stiroy@users.noreply.github.com>
2023-02-24 15:22:20 +0100

    -added descriptions
    added descriptions to the functions and at some points in the code

--- GetsOverflow.c
+++ GetsOverflow.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
+// this program shows a simple program with a stack overflow 
+// exploit. by writting AAAAxAAAAx1 will you pass the validation
+
 void printAdressesForString(char*);
 
 int main (){
+    // Variables
     char secretCode[5] = "pass";
     char inputData[5];
-    int pass = 0;
+    int pass = 0; // check variable
 
     printf("Please Write in the code\n");
     gets(inputData);
@@ -29,7 +33,11 @@ int main (){
 
     return 0;
 }
-
+/**
+ * prints the memory and the character until null is found
+ * @param str char pointer to the memory space
+ * @todo Better text out put for easier readablity
+*/
 void printAdressesForString(char* str){
     printf("Memory for String\n");
     for (char* stringPointer = str ; *stringPointer != '\0' ; stringPointer += sizeof(char)){
