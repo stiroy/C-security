0ea286c5b1044b123eeb75e72d7af680549605cd
stiroy <55147197+stiroy@users.noreply.github.com>
2023-02-24 15:26:41 +0100

    -Change & update
    change the scanf_s function due to gcc update not done yet and add det more desriptions

--- scanfOverflow.c
+++ scanfOverflow.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
+// this Prgoram shows that it's possiable to do 
+// stack overflow on scanf 
+// input to pass the check is AAAAx1
+
 int main(){
-    char* buffer;
+    //variables
+    char buffer[5];
     int check = 0;
 
     printf("enter text: ");
-    scanf_s("%s", buffer);
+    scanf("%s", buffer); // the problem is scanf format that isn't a set amount like %4s
     if (check)
     {
         printf("Passed!");
