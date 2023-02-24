18ccdb2b5b3c35dfddd06282ba559ca4667c4b91
stiroy <55147197+stiroy@users.noreply.github.com>
2023-02-24 15:08:13 +0100

    Update ThreadWindows.c
    -add description

--- ThreadWindows.c
+++ ThreadWindows.c
@@ -3,13 +3,15 @@
 
 //this is an example of code that cause a infinity loop if you optimaze it
 
+// function that waits 1 second before changing the value of the 
+// variable
 DWORD WINAPI Func(void* ptr){
     sleep(1);
     *(int*)ptr = 0;
 }
 
 int main(){
-    int wait = 1;
+    int wait = 1; 
     printf("please wait while your getting auth\n");
     HANDLE thread = CreateThread(NULL, 0, Func, &wait, 0, NULL);
     while (wait);
